a21168e834141165ab9a9adf12420c4ff6d9570e
cfmaca18 <102422974+cfmaca18@users.noreply.github.com>
2022-11-12 20:22:43 -0500

    Update and rename proyectos/urls.py to Proyectos/urls.py
    ViewSet restantes añadidas.

---
+++ Proyectos/urls.py
@@ -0,0 +1,23 @@
+from django.urls import include, path
+from rest_framework import routers
+
+from proyectos.views.rol import *
+from proyectos.views.regional import *
+from proyectos.views.ficha import *
+from proyectos.views.centros_de_formacion import *
+from proyectos.views.programa import *
+
+
+router = routers.DefaultRouter()
+
+router.register(r'rol', RolViewSet)
+router.register(r'regional', RegionalViewSet)
+router.register(r'ficha', FichaViewSet)
+router.register(r'centro', Centros_de_formacionViewSet)
+router.register(r'programa', ProgramaViewSet)
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
+urlpatterns = [
+    path('', include(router.urls)),
+]
